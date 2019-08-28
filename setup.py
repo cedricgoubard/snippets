c889ed1914d25f2d858e582982f37b51b4b335b8
Cédric Goubard <40461909+cedricgoubard@users.noreply.github.com>
2019-08-28 10:02:42 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='MY_PACKAGE_NAME',
+    version='0.0.1',
+    description='MY DESCRIPTION',
+    author='Cédric Goubard',
+    packages=find_packages()
+)
