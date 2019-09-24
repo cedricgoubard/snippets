35329a16ce644b4e4bb99446d1dc13833a2ccc03
Cédric Goubard <40461909+cedricgoubard@users.noreply.github.com>
2019-09-24 10:45:46 +0200

    Add args in git powershell script

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,20 @@
-function get-gitstatus { git status -sb }
+function get-gitstatus { git status -sb $args }
 Set-Alias -Name g -Value get-gitstatus
 
-function get-githist { git hist }
+function get-githist { git hist $args }
 Set-Alias -Name gh -Value get-githist
 
-function gitadd { git add }
+function gitadd { git add $args }
 Set-Alias -Name ga -Value gitadd
 
-function gitpull { git pull }
-Set-Alias -Name gp -Value gitpull
+function gitpull { git pull $args }
+Set-Alias -Name gpl -Value gitpull
 
-function gitbranch { git branch }
+function gitbranch { git branch $args }
 Set-Alias -Name gb -Value gitbranch
 
-function gitcommit { git commit }
-Set-Alias -Name gc -Value gitcommit
+function gitcommit { git commit $args }
+Set-Alias -Name gco -Value gitcommit
 
-function gitshow { git show }
+function gitshow { git show $args }
 Set-Alias -Name gs -Value gitshow
